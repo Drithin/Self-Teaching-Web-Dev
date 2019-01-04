c8507e65a1d1d3bd227255c11a56540aff02ad46
Drithin <drithn@gmail.com>
2019-01-04 13:19:18 +0530

    Implemented do while loop to get the prompt repeatedly. it was successful. In the next iteration, target is to add attempts factor to this game. i.e, if 3rd attempt is not successful, it'll print game over.

--- Javascript/Guessing-Game/guessing-game.js
+++ Javascript/Guessing-Game/guessing-game.js
@@ -1,18 +1,23 @@
 var secretNum = 5;
 
-var guessNum = Number(prompt("Enter the Number you guessed"));
+do{
+	var guessNum= Number(prompt("Enter the Number you guessed"));
+} while (guessNum !== secretNum);
+
+alert("You Guessed it right. The number is " + secretNum);
 
 
 
-	 if(guessNum < secretNum){
-		prompt("Number is too low");
-	}
-	else if(guessNum > secretNum){
-		prompt("Number is too high!");
-	}
-	else{
-		alert("You guessed it right ");
-	}
+
+	//  if(guessNum < secretNum){
+	// 	prompt("Number is too low");
+	// }
+	// else if(guessNum > secretNum){
+	// 	prompt("Number is too high!");
+	// }
+	// else{
+	// 	alert("You guessed it right ");
+	// }
 
 
 		

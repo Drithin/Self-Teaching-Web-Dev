2127f68680459dbc02bdf477d7dd635c43a01a6e
Drithin Chakravarthy Nellore <drithn@gmail.com>
2019-04-12 11:04:01 +0530

    although achieved the desired functionality of text changing color with clicking, code can be compact and optimised further. With eventual scaling in JS, I am sure I'll achieve it

---
+++ Javascript/Color-Picker/script.js
@@ -0,0 +1,11 @@
+function colorChange1(){
+	document.getElementById("text-color-change").style.color = "red";
+}
+
+function colorChange2(){
+	document.getElementById("text-color-change").style.color = "blue";
+}
+
+function colorChange3(){
+	document.getElementById("text-color-change").style.color = "green";
+}

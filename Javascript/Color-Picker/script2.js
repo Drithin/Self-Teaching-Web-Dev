cc62f60ebaf3f067859e16cde850870e0ad8aed0
Drithin Chakravarthy Nellore <drithn@gmail.com>
2019-04-12 11:04:52 +0530

    fully optimised version of js code. I coded a rough idea of what it can be. Will fine tune it soon

---
+++ Javascript/Color-Picker/script2.js
@@ -0,0 +1,13 @@
+
+
+let colors = ['red', 'blue', 'green'];
+
+function colorChanges(){
+	
+	for(var i = 0; i < colors.length; i++){
+// 	document.getElementById("text-color-change").style.color = "`${colors[i]`";
+console.log(colors[i]);
+ }
+}
+
+colorChanges();

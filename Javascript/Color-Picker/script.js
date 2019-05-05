9ae0287dfb091152103fb83c5d21a5143f437771
Drithin Chakravarthy Nellore <drithn@gmail.com>
2019-05-05 13:09:30 +0530

    hard coded the new colors into the script

--- Javascript/Color-Picker/script.js
+++ Javascript/Color-Picker/script.js
@@ -9,3 +9,22 @@ function colorChange2(){
 function colorChange3(){
 	document.getElementById("text-color-change").style.color = "green";
 }
+
+function colorChange4(){
+	document.getElementById("text-color-change").style.color = "magenta";
+}
+
+function colorChange5(){
+	document.getElementById("text-color-change").style.color = "moccasin";
+}
+
+function colorChange6(){
+	document.getElementById("text-color-change").style.color = "tomato";
+}
+
+
+
+
+
+
+

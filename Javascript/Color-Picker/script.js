b679a9309ccdd5d5dd673b6009a81106d99a60c8
Drithin Chakravarthy Nellore <drithn@gmail.com>
2019-05-09 11:01:22 +0530

    although hard coded, achieved desired functionality of changing text to corresponding color. The function can be more concise. will eventually work towards it.

--- Javascript/Color-Picker/script.js
+++ Javascript/Color-Picker/script.js
@@ -1,25 +1,31 @@
 function colorChange1(){
 	document.getElementById("text-color-change").style.color = "red";
+	document.getElementById("h1").innerHTML = "Red";
 }
 
 function colorChange2(){
 	document.getElementById("text-color-change").style.color = "blue";
+	document.getElementById("h1").innerHTML = "Blue";
 }
 
 function colorChange3(){
 	document.getElementById("text-color-change").style.color = "green";
+	document.getElementById("h1").innerHTML = "Green";
 }
 
 function colorChange4(){
 	document.getElementById("text-color-change").style.color = "magenta";
+	document.getElementById("h1").innerHTML = "Magenta";
 }
 
 function colorChange5(){
 	document.getElementById("text-color-change").style.color = "moccasin";
+	document.getElementById("h1").innerHTML = "Moccasin";
 }
 
 function colorChange6(){
 	document.getElementById("text-color-change").style.color = "tomato";
+	document.getElementById("h1").innerHTML = "Tomato";
 }
 
 
